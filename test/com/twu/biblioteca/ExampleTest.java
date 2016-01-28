1f0ea396ba8c849cff9f6444bf2ac80fa36a2385
slizeng <251231296@qq.com>
2016-01-28 21:15:11 +0800

    add test for system.out

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,32 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
     @Test
     public void test() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void testWelcome() throws Exception {
+        BibliotecaApp biliotecaApp = new BibliotecaApp();
+        System.out.println("hello,world!");
+        assertEquals("hello,world!\n", outContent.toString());
     }
 }
