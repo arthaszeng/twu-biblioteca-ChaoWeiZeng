23147568fb1c281d4bf56b385111694e6737b1ce
slizeng <251231296@qq.com>
2016-01-29 00:54:15 +0800

    separated ConsoleOutputTest from ExampleTest

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,53 +1,15 @@
 package com.twu.biblioteca;
 
 
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
     @Test
     public void test() {
         assertEquals(1, 1);
-    }
-
-    @Test
-    public void testWelcome() throws Exception {
-        BibliotecaApp biliotecaApp = new BibliotecaApp();
-
-        System.out.println("hello,world!");
-        assertEquals("hello,world!\n", outContent.toString());
-    }
-
-    @Test
-    public void testShowBooksAfterWelcome() throws Exception {
-        BibliotecaApp biliotecaApp = new BibliotecaApp();
-        biliotecaApp.AppInit();
-        biliotecaApp.showBooks();
-
-        assertEquals("<<Lean Thinking>>\tJames P. Womack\t2003.06.01\n" +
-                "<<Clean Code>>\tRobert C. Martin\t2010.01.01\n", outContent.toString());
-    }
-
-    @Test
-    public void testShowBookDetails() throws Exception {
-        BibliotecaApp biliotecaApp = new BibliotecaApp();
-        Books leanThinking = new Books("Lean Thinking", "James P. Womack", "2003.06.01");
-        biliotecaApp.showOneBook(leanThinking);
-
-        assertEquals("<<Lean Thinking>>\tJames P. Womack\t2003.06.01\n" ,outContent.toString());
     }
 
 
